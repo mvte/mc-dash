775390f2bef95d1eadc24a1d733cc4323f022889
mvte <janmarzan11@gmail.com>
2023-06-12 17:35:24 -0400

    add login page logic

---
+++ client/src/components/SignIn/index.js
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+
+const SignIn = (props) => {
+
+    const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+
+	async function loginUser(event) {
+        event.preventDefault();
+
+		const response = await fetch('http://localhost:9000/api/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				username,
+				password,
+			}),
+		})
+
+		const data = await response.json()
+
+		if (data.user) {
+			localStorage.setItem('token', data.user)
+			window.location.href = '/home'
+		} else {
+			alert('your username or password is incorrect')
+		}
+
+        event.preventDefault();
+	}
+
+    return (
+        <div>
+            <h1>hello</h1>
+            <form onSubmit={loginUser}>
+                <input 
+                    value={username}
+					onChange={(e) => setUsername(e.target.value)}
+                    name="username" 
+                    type="text" 
+                    placeholder="username" 
+
+                    />
+                <input 
+                    value={password}
+					onChange={(e) => setPassword(e.target.value)}
+                    name="password" 
+                    type="password" 
+                    placeholder="password"
+                />
+                <button type="submit">sign in</button>
+            </form>
+        </div>
+    )
+}
+
+export default SignIn;
