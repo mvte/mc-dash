1ac0e8c25756c77d5e64f85958443b79da64e8bf
mvte <janmarzan11@gmail.com>
2023-06-17 01:09:45 -0400

    unify server and socket under one port

--- server/server.js
+++ server/server.js
@@ -1,42 +1,49 @@
 require('dotenv').config();
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server, {
-  cors: {
-    origin: '*',
-  }
-});
-
-const mongoose = require('mongoose');
 const path = require('path');
 
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
+//create server and socket
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+  path: '/api/performance/stream',
+});
+
+//connect to db
+const mongoose = require('mongoose');
+
+//declare routes
 const auth = require('./routes/auth');
 const info = require('./routes/info');
 const performance = require('./routes/performance');
+const streamRoute = require('./routes/streams');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
-// link routes
+//link routes
 app.use('/api', auth);
 app.use('/api/info', info);
 app.use('/api/performance', performance);
-const streamRoute = require('./routes/streams');
 streamRoute(io);
 
 const port = process.env.PORT || 9000;
 
-
 app.get('/api', (req, res) => {
-  res.send({ message: 'this is a test message' });
+  res.send({ message: 'this is a test message'});
 });
-
 
 // unknown routes should be handled by react
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname,'..', 'client', 'build', 'index.html'));
 });
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
-server.listen(port+1, () => console.log(`Socket listening on port ${port+1}`));
+httpServer.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
