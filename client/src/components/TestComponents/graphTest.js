cdb1f535d894b7e1d2bdfcf573e6e08bbabb437f
mvte <janmarzan11@gmail.com>
2023-06-17 01:10:20 -0400

    use default domain

--- client/src/components/TestComponents/graphTest.js
+++ client/src/components/TestComponents/graphTest.js
@@ -1,13 +1,15 @@
 //this component shows you how to access the performance stream from the backend
 import { useState, useEffect } from "react";
-import socketIOClient from "socket.io-client"; //this is the library that allows us to connect to the socket
+import io from "socket.io-client"; //this is the library that allows us to connect to the socket
 
 const GraphTest = (props) => {
     const [data, setData] = useState("loading..."); //the useState hook allows us to re-render only specific components when a variable changes
 
     useEffect(() => {
         console.log('connecting to socket');
-        const socket = socketIOClient('http://localhost:9001'); //note that the port is 9001, not 9000
+        const socket = io({ //this is how you connect to the data stream
+          path:'/api/performance/stream', //this path is necessary to connect to the stream
+        }); 
     
         socket.on('streamUpdate', (data) => { //this socket is listening for a streamUpdate event, which i have defined in the backend
           setData(data);  //when the event is received, the data is set to the state variable   
