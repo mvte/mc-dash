30caf765a602cc34725f80fe2bd0f60c3c21b1ca
mvte <janmarzan11@gmail.com>
2023-06-18 13:13:33 -0400

    add console stream

---
+++ server/streams/console.js
@@ -0,0 +1,46 @@
+const Docker = require('dockerode');
+const fs = require('fs');
+
+const docker = new Docker({
+    protocol: 'https',
+    host: process.env.DOCKER_HOST,
+    port: 2376,
+    ca: fs.readFileSync('../certs/ca.pem'),
+    cert: fs.readFileSync('../certs/cert.pem'),
+    key: fs.readFileSync('../certs/key.pem')
+});
+
+let container = docker.getContainer(process.env.CONTAINER_ID);
+
+module.exports = function (io) {
+    io.on('connection', (socket) => {
+        console.log('client connected to logs stream');
+
+        container.logs({ follow: true, stdout: true, stderr: true }, (err, stream) => {
+            if (err) {
+                console.log(err);
+            } else {
+                stream.on('data', (chunk) => {
+                    const data = chunk.toString();
+                    socket.emit('streamUpdate', {
+                        data: data,
+                    });
+                });
+                
+                stream.on('error', () => {
+                    console.log('error');
+                });
+
+                stream.on('disconnect', () => {
+                    console.log('stream ended');
+                    stream.destroy();
+                });
+            }
+        });
+
+        socket.on('disconnect', () => {
+            console.log('client disconnected');
+        });
+    });
+}
+    
