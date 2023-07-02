6be47aa73e1f7d1c806b27358877715f7f216a13
mvte <janmarzan11@gmail.com>
2023-07-02 16:18:59 -0400

    update login route + add register route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,41 +1,103 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const mongoose = require('mongoose');
 const router = express.Router();
 
-router.post('/login', async (req, res) => {
-	// const user = await User.findOne({
-	// 	username: req.body.username,
-	// })
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 
-	// if (!user) {
-	// 	return { status: 'error', error: 'Invalid login' }
-	// }
+const mongoose = require('mongoose');
+const userSchema = require('../model/User');
+const User = mongoose.model('User', userSchema);
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
-	// const isPasswordValid = await bcrypt.compare(
-	// 	req.body.password,
-	// 	user.password
-	// )
-  const { username, password } = req.body
-  const roles = ['admin', 'user']
 
-  isPasswordValid = password === 'password' && username === 'admin'
+router.post('/register', async (req, res) => {
+	const { username, password: plainTextPassword } = req.body
 
-	if (isPasswordValid) {
+	if (!username || typeof username !== 'string' || !isAlphaNumeric(username)) {
+		return res.json({ ok: false, error: 'username must be alphanumeric' })
+	}
+
+	bcrypt.hash(plainTextPassword, saltRounds, async (err, hash) => {
+		const user = new User({
+			username: username,
+			password: hash,
+			admin: false,
+		});
+		try {
+			const response = await user.save()
+			console.log('user created successfully: ', response)
+		} catch (error) {
+			if (error.code === 11000) {
+				// duplicate key
+				return res.json({ ok: false, error: 'username already exists' })
+			}
+			throw error
+		}
+
 		const token = jwt.sign(
 			{
 				name: username,
-				roles: roles
+				roles: ['user']
 			},
 			process.env.JWT_SECRET,
 			{ expiresIn: '24h' }
-		)
-
-		return res.json({ ok: true, token: token })
-	} else {
-		return res.json({ ok: false, token: false })
-	}
+		);
+		res.json({ ok: true, token: token })
+	});
 });
+
+router.post('/login', async (req, res) => {
+	const { username, password } = req.body
+	User.findOne({ username: username })
+	.then(user => {
+		if(!user) {
+			return res.json({ ok: false, error: 'username or password is incorrect' });
+		}
+		const roles = user.admin ? ['admin', 'user'] : ['user'];
+
+		bcrypt.compare(password, user.password, (err, result) => {
+			if(err) {
+				console.log(err);
+				return res.json({ ok: false, error: 'something went wrong please try again' });
+			}
+
+			if(result) {
+				const token = jwt.sign(
+					{
+						name: username,
+						roles: roles,
+					},
+					process.env.JWT_SECRET,
+					{ expiresIn: '24h' }
+				);
+				return res.json({ ok: true, token: token });
+			} else {
+				return res.json({ ok: false, error: 'username or password is incorrect' });
+			}
+		});
+	})
+	.catch(err => {
+		console.log(err);
+		return res.json({ ok: false, error: 'something went wrong please try again' });
+	});
+});
+
+function isAlphaNumeric(str) {
+	let code, i, len;
+
+	for (i = 0, len = str.length; i < len; i++) {
+		code = str.charCodeAt(i);
+
+		if (!(code > 47 && code < 58) && // numeric (0-9)
+			!(code > 64 && code < 91) && // upper alpha (A-Z)
+			!(code > 96 && code < 123)) { // lower alpha (a-z)
+			return false;
+		}
+	}
+
+	return true;
+}
+
 
 module.exports = router;
