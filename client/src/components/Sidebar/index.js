79f35232a06d2ca1fe6eb61126bc8c82234e2ac3
mvte <janmarzan11@gmail.com>
2023-08-02 00:52:20 -0400

    add sidebar

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,33 +1,61 @@
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import React, { useState } from 'react';
+import  { Link } from 'react-router-dom';
+import * as BsIcons from 'react-icons/bs';
 import './index.css';
 
+const Sidebar = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const iconSize = 20;
 
-const Sb = (props) => {
+    const handleToggleCollapse = () => {
+        setCollapsed(!collapsed);
+    };
+  
+    const menuItems = [
+        {
+            name: 'home',
+            path: '/home',
+            icon: <BsIcons.BsHouseDoorFill size={iconSize}/>,
+        },
+        {
+            name: 'console',
+            path: '/console',
+            icon: <BsIcons.BsTerminalFill size={iconSize}/>,
+        },
+        {
+            name: 'settings',
+            path: '/settings',
+            icon: <BsIcons.BsGearFill size={iconSize}/>,
+        },
+        {
+            name: 'map',
+            path: '/map',
+            icon: <BsIcons.BsMap size={iconSize}/>,
+        },
+        {
+            name: 'logout',
+            path: '/logout',
+            icon: <BsIcons.BsBoxArrowRight size={iconSize}/>,
+        }
+    ];
+
+
     return (
-        <div className="sidebar">
-            <Sidebar
-                title={props.title}
-                collapsed={props.collapsed}
-                backgroundColor='#121212'
-                rootStyles={{
-                    color: '#121212',
-                    backgroundColor: '#121212',
-                    height: '100vh',
-                }}
-            >
-                <Menu
-                    menuItemStyles={{
-                        backgroundColor: '#121212',
-                    }}
-                >
-                    <SubMenu title="Dashboard">
-                        <MenuItem>Home</MenuItem>
-                        <MenuItem>Test</MenuItem>
-                    </SubMenu>
-                </Menu>
-            </Sidebar>
+        <div className={`sidebar ${collapsed ? 'collapsed' : ''}`}>
+            <button className={`toggle-button ${collapsed ? 'collapsed' : ''}`} onClick={handleToggleCollapse}>
+                {collapsed ? <BsIcons.BsChevronRight size={iconSize}/> : <>menu <BsIcons.BsChevronLeft size={iconSize}/></>}
+            </button>
+            <ul className="menu-items">
+                {menuItems.map((item, index) => (
+                    <li key={index} className="menu-item">
+                    <Link to={item.path}>
+                        {collapsed ? item.icon : item.name}
+                    </Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
-}
+ };
 
-export default Sb;
+export default Sidebar;
