aaa11885dbe1c2348797f1fbb21216b016e51661
mvte <janmarzan11@gmail.com>
2023-09-22 23:40:43 -0400

    add card layout

---
+++ client/src/components/Cards/CardConsts.js
@@ -0,0 +1,20 @@
+import { styled } from "@mui/material/styles";
+
+const Card = styled('div')`
+    background-color: #2c2f33;
+    border-radius: 10px;
+    height: 280px;
+    padding: 20px;
+    margin: 20px;
+    box-shadow: 0 0 10px rgba(0,0,0,0.5);
+    color: #fff;
+    font-size: 1.2rem;
+    font-weight: 500;
+    text-align: left;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: left;
+`;
+
+export default Card;
