1c354190d7d131ec4ac54c93413ab4023c2bf28b
mvte <janmarzan11@gmail.com>
2023-10-07 01:08:13 -0400

    update certs routes

--- server/routes/container.js
+++ server/routes/container.js
@@ -10,9 +10,9 @@ const docker = new Docker({
     protocol: 'https',
     host: process.env.DOCKER_HOST,
     port: 2376,
-    ca: fs.readFileSync('../certs/ca.pem'),
-    cert: fs.readFileSync('../certs/cert.pem'),
-    key: fs.readFileSync('../certs/key.pem')
+    ca: fs.readFileSync('certs/ca.pem'),
+    cert: fs.readFileSync('certs/cert.pem'),
+    key: fs.readFileSync('certs/key.pem')
 });
 
 let container = docker.getContainer(process.env.CONTAINER_ID);
