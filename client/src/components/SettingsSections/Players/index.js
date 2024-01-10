405e4c584a765781dc42602ee6dfb50baf144717
mvte <janmarzan11@gmail.com>
2024-01-09 19:55:03 -0500

    add placeholders for properties and players panels

---
+++ client/src/components/SettingsSections/Players/index.js
@@ -0,0 +1,11 @@
+
+const Players = () => {
+
+    return (
+        <div>
+            <h1>players</h1>
+        </div>
+    )
+}
+
+export default Players;
---
+++ client/src/components/SettingsSections/Properties/index.js
@@ -0,0 +1,11 @@
+
+const Properties = () => {
+
+    return (
+        <div>
+            <h1>properties</h1>
+        </div>
+    )
+}
+
+export default Properties;
