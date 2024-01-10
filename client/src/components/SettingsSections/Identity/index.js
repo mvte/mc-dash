7876535d7384bc4b00499959636147af301c4068
mvte <janmarzan11@gmail.com>
2024-01-09 19:53:44 -0500

    foundations for server identity panel

---
+++ client/src/components/SettingsSections/Identity/index.js
@@ -0,0 +1,39 @@
+import { Panel, H1, P, SettingsInput, SettingsButton } from '../SettingsConsts'
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import Grid from '@mui/material/Grid';
+
+const Identity = (props) => { 
+
+    return (
+        <div>
+            <H1>identity</H1>
+            <Panel>
+                <Grid container spacing={2} alignItems="center">
+                    <Grid item xs={2} >
+                        <P> message of the day </P>
+                    </Grid>
+                    <Grid item xs={10}>
+                        <SettingsInput
+                            id="motd"
+                            placeholder="placeholder motd will be the server's actual motd" // get from props
+                            fullWidth
+                            size="small"
+                            autoComplete="off"
+                        />
+                    </Grid>
+                    <Grid item xs={2}>
+                        <P> server icon </P>
+                    </Grid>
+                    <Grid item xs={10} sx = {{display: "flex", alignItems: "center", gap: "24px"}}>
+                        <SettingsButton variant="contained" startIcon={<CloudUploadIcon />}>
+                            upload
+                        </SettingsButton>
+                        <img src="https://placehold.co/64" alt="server icon"/>
+                    </Grid>
+                </Grid>
+            </Panel>
+        </div>
+    )
+}
+
+export default Identity;
