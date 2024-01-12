dd4cabb01eb51163547c477ddd3cc1c2c5bb505a
mvte <janmarzan11@gmail.com>
2024-01-11 23:37:42 -0500

    create custom combo box component for settings page

--- client/src/components/SettingsSections/SettingsConsts.js
+++ client/src/components/SettingsSections/SettingsConsts.js
@@ -1,6 +1,7 @@
 import { styled } from "@mui/material/styles";
 import InputBase from '@mui/material/InputBase';
-import { Button, Autocomplete, TextField } from "@mui/material";
+import { Button, Autocomplete, InputAdornment } from "@mui/material";
+import { ArrowDropDown } from '@mui/icons-material';
 
 const Panel = styled('div')`
     background-color: #2c2f33;
@@ -19,7 +20,7 @@ const P = styled('p')`
     padding: 0; 
 `
 
-const SettingsInput = styled(TextField)({
+const SettingsInput = styled(InputBase)({
     '& .MuiInputBase-input': {
         color: '#1E1E1E',
         backgroundColor: 'white',
@@ -33,8 +34,32 @@ const SettingsButton = styled(Button)`
     text-transform: none;
 `
 
-const ComboBox = styled(Autocomplete)({
+const StyledInputAdornment = styled(InputAdornment)({
+    position: 'absolute',
+    right: 0,
+});
 
-})
+const ComboBox = ({placeholder, ...props}) => {
+    return <Autocomplete  
+        {...props}
+        noOptionsText="womp womp"
+        renderInput={(params) => {
+            const { InputProps, InputLabelProps, ...rest } = params;
 
-export { Panel, H1, P, SettingsInput, SettingsButton, ComboBox };
+            return <SettingsInput
+                {...rest}
+                placeholder={placeholder}
+                inputRef={InputProps.ref}
+                inputProps={{...rest.inputProps}}
+                endAdornment={
+                    <StyledInputAdornment position="end">
+                        <ArrowDropDown style={{ color: "#1e1e1e", pointerEvents: "none", overflow: "visible" }}/>
+                    </StyledInputAdornment>
+                }
+            />
+        }}
+    />
+}
+
+
+export { Panel, H1, P, SettingsInput, SettingsButton, StyledInputAdornment, ComboBox };
